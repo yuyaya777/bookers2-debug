23afd3546ce7f4404f50f60c229d9108c3c99f31
yuyaya777 <yuyaya777@gmail.com>
2021-04-03 09:17:50 +0000

    Commit2

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,7 @@ class ApplicationController < ActionController::Base
 
   protected
   def after_sign_in_path_for(resource)
-    root_path
+    user_path(resource)
   end
 
   def after_sign_out_path_for(resource)
